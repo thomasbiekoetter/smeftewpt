2346c05f6ce79a595e5fd7093d68c7266b449360
Thomas <thomas.biekoetter@desy.de>
2026-06-18 17:45:11 +0200

    Implemented LO 3D EFT potential.

--- src/smeftewpt/potentials/threedim.py
+++ src/smeftewpt/potentials/threedim.py
@@ -17,7 +17,7 @@ class ThreeDim:
 
         self.LambdaUV = LambdaUV
         self.mu_match_fac = mu_match_fac
-        self.CH = CH
+        self.CH = CH / LambdaUV ** 2
         self.Ckin = 0.0
         self.CtH = 0.0
         self.calc_internal_paras_atMZ()
@@ -61,10 +61,9 @@ class ThreeDim:
 
         # We treat here the SMEFT Wilsons at mu = MZ (neglect their running). Consistent?
 
-        self.musq_MZ =  Mh ** 2 / 2. + (3 * self.CH * self.vev ** 4) / (4. * self.LambdaUV ** 2)
+        self.musq_MZ = Mh ** 2 / 2. + (3 * self.CH * self.vev ** 4) / 4.0
 
-        self.lam_MZ = Mh ** 2 / (2. * self.vev ** 2) + (3 * self.CH * self.vev ** 2) /  \
-            (2. * self.LambdaUV ** 2)
+        self.lam_MZ = Mh ** 2 / (2. * self.vev ** 2) + (3 * self.CH * self.vev ** 2) / 2.0
 
 
     def make_running_4D(self, scale_max=1e3):
@@ -74,37 +73,66 @@ class ThreeDim:
             lam=self.lam_MZ, m2=self.musq_MZ)
 
 
-    def make_hard_matching(self, T):
+    def make_hard_matching(self, T, mode="O(g^4)"):
         # First get 4D parameters at scale mu = mu_match_fac * pi * T
         mu = self.mu_match_fac * np.pi * T
         yt = self.rge.get_yt(mu)
         lam = self.rge.get_lam(mu)
         musq = self.rge.get_m2(mu)
+        g1 = self.rge.get_gY(mu)
+        g2 = self.rge.get_g2(mu)
         # SMEFT coefficients (no running yet)
         CH = self.CH
         # To be checked
-        msq3D = -musq # + ...
-#       cth = self.lam / 4.0 + 3.0 * self.g2**2  / 16.0  \
-#           + self.g1**2 / 48.0 + self.t**2 / 12.0
-#       self.musq3D = -self.musq + cth * T ** 2
-#       self.lam3D = self.lam * T
-#       self.CH3D = self.CH * T
+        if mode == "O(g^4)":
+            Tsq = T ** 2
+            # CH term not contained in Eq. (52) of 2503.20016
+            self.msq3D = -musq + Tsq * (g1 ** 2 + 3 * g2 ** 2 +
+                8 * lam - 4 * CH * Tsq + 4 * yt ** 2) / 16.0
+            # Compared to Eq. (52) of 2503.20016 I miss terms ~ g1, g2
+            self.lam3D = lam * T - CH * T ** 3
+            self.C3D = -CH * Tsq
+        elif mode == "LO_Chala":
+            # Eq. 52 of 2503.20016
+            Tsq = T ** 2
+            self.msq3D = -musq + Tsq * (g1 ** 2 + 3 * g2 ** 2 +
+                8 * lam + 4 * yt ** 2) / 16.0
+            self.lam3D = lam * T - CH * T ** 3 + T * (
+                g1 ** 4 + 2 * g1 ** 2 * g2 ** 2 + 3 * g2 ** 4) / (128.0 * np.pi ** 2)
+            self.C3D = -CH * Tsq
 
 
-    def Veff(self, h, T):
-        self.make_hard_matching(T)
-#       y = self.V0(h) + self.V1(h)
-#       return y
+    def Veff(self, h, T, mode="O(g^4)"):
+        self.make_hard_matching(T, mode=mode)
+        h = h / np.sqrt(T)
+        y = self.V0(h, T)
+        y += self.V1(h)
+        y += self.V2(h)
+        return y * T
 
 
-#   def Vtree(self, h):
-#       # Prefactors to be checked
-#       y = 0.5 * self.musq3D * h ** 2 +  \
-#           0.25 * self.lam3D * h ** 4 -  \
-#           (1.0 / 6.0) * 0.75 * self.CH3D * h ** 6
-#       return y
+    def V0(self, h, T):
+        # Prefactors such that consistent with Eq. (52) of 2503.20016
+        # The prefactor for the h^6 term is inconsistent with the
+        # definition of the phi^6 term in Eq. 7 of 2503.20016,
+        # but if I don't switch its sign, then the potential is not BfB
+        # for negative CH. But CH has to be negative because otherwise
+        # the 4D potential is not BfB, see definition of the phi^6
+        # term in Eq. 2 of 2503.20016
+        y = self.msq3D * h ** 2 +  \
+            self.lam3D * h ** 4 +  \
+            self.C3D * h ** 6
+        y = y
+        return y
 
 
-#   def V1(self, h):
-#       y = 0.0
-#       return y
+    def V1(self, h):
+        # To be implemented if necessary
+        y = 0.0
+        return y
+
+
+    def V2(self, h):
+        # To be implemented if necessary
+        y = 0.0
+        return y
